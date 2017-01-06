80485aade4b7f2f03f1d20c7ce49b6ee16dafc82
leahsass <sass.leahm@gmail.com>
2017-01-06 22:29:37 +0200

    ready to pull master

--- JS/landingPage.js
+++ JS/landingPage.js
@@ -1,8 +1,26 @@
 var landingPage = {};
 
 
+//show background image
+$(document).ready(function() {
+    $("#background-overlay").show();
+    $('#container-world').hide();
+    $('#container-toolbar').hide();
+});
+
+
 //new game button
 landingPage.newGameButton = function() {
-    var newGame = $("<div/>");
+    var newGame = $("<button/>");
+    newGame.addClass("new-game-button");
+    newGame.text("New Game");
+    $("body").append(newGame);
+
+    //when game button is clicked, fade out image, show world and toolbar
+    $("#background-overlay").on('click', function () {
+        $('#wrapper_index').fadeOut();
+        $('#container-world').show();
+        $('#container-toolbar').show();
+});
 
 }
