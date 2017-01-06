a7992fdbc46ad805c311d5cc3d6431a6c7b4a3f5
leahsass <sass.leahm@gmail.com>
2017-01-06 22:58:31 +0200

    ready to push toolbar

--- JS/landingPage.js
+++ JS/landingPage.js
@@ -1,26 +1,23 @@
-var landingPage = {};
+// var landingPage = {};
 
 
-//show background image
-$(document).ready(function() {
-    $("#background-overlay").show();
-    $('#container-world').hide();
-    $('#container-toolbar').hide();
-});
+// //show background image
+// $(document).ready(function() {
+//     $("#background-overlay").show();
+//     $("#container-world").hide();
+//     $("#container-toolbar").hide();
+// });
 
 
-//new game button
-landingPage.newGameButton = function() {
-    var newGame = $("<button/>");
-    newGame.addClass("new-game-button");
-    newGame.text("New Game");
-    $("body").append(newGame);
+// //new game button
+// landingPage.newGameButton = function() {
+    
+//     //when game button is clicked, fade out image, show world and toolbar
+//     $("button").click(function () {
+//         $("html").fadeOut();
+//         $("#background-overlay").fadeOut();
+//         $("#container-world").show();
+//         $("#container-toolbar").show();
+// });
 
-    //when game button is clicked, fade out image, show world and toolbar
-    $("#background-overlay").on('click', function () {
-        $('#wrapper_index').fadeOut();
-        $('#container-world').show();
-        $('#container-toolbar').show();
-});
-
-}
+// }
